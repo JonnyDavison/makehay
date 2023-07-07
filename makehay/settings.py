0f7acc1b095a501f83379790cda496e4fcc6754b
JonnyDavison <jonnydavison@icloud.com>
2023-07-07 19:18:06 +0000

    Add Baron Neue font fam

--- makehay/settings.py
+++ makehay/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['makehay-bb96cb74e437.herokuapp.com']
-# ALLOWED_HOSTS = ['8000-jonnydavison-makehay-oh730kgiddk.ws-eu101.gitpod.io']
+# ALLOWED_HOSTS = ['makehay-bb96cb74e437.herokuapp.com']
+ALLOWED_HOSTS = ['8000-jonnydavison-makehay-oh730kgiddk.ws-eu101.gitpod.io']
 
 # Application definition
 
@@ -157,11 +157,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
